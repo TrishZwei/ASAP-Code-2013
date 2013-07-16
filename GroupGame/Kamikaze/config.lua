5e86119f89de418706cf82eacb2a235e62196147
Trish Ladd <trishzwei@gmail.com>
2013-07-15 23:08:01 -0700

    Additional Resources
    Build, config, buttons, ocean, ocean background, ship placeholder, plane
    placeholder, buttons are all part of this commit.

---
+++ GroupGame/Kamikaze/config.lua
@@ -0,0 +1,16 @@
+application =
+{
+
+	content =
+	{
+		--size of an original iphone
+		width=320,
+		height = 480,
+		--letterbox scales as uniformly as possible
+		--will create a widescreen compatible with the droid which has a longer screen
+		scale = "letterbox",
+		--maximum fps corona will allow, default is 30
+		fps = 60,
+	},	
+
+}
